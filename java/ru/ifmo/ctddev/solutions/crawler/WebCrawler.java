6b456ddba47fa32cee7d5e291f789ffc6cec3c38
BlitzAL <34640386+BlitzAL@users.noreply.github.com>
2018-01-25 00:09:53 +0300

    Update WebCrawler.java

--- java/ru/ifmo/ctddev/solutions/crawler/WebCrawler.java
+++ java/ru/ifmo/ctddev/solutions/crawler/WebCrawler.java
@@ -1,104 +1,99 @@
 package ru.ifmo.ctddev.solutions.crawler;
 
+import info.kgeorgiy.java.advanced.crawler.CachingDownloader;
+import info.kgeorgiy.java.advanced.crawler.Crawler;
 import info.kgeorgiy.java.advanced.crawler.Document;
 import info.kgeorgiy.java.advanced.crawler.Downloader;
 import info.kgeorgiy.java.advanced.crawler.Result;
-
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
-import static info.kgeorgiy.java.advanced.crawler.URLUtils.getHost;
 
-public class AddMethod {
-    public static Result Res = new Result(Collections.emptyList(), Collections.emptyMap());
-    public static  int ExtrLim = 140;
-    public static  int DwnlLim = 140;
-    public Map<String, Lock> DwnldLocks = new ConcurrentHashMap<>();
-    public  Map<String, Semaphore> HostC = new ConcurrentHashMap<>();
-    public Downloader Dwnldr;
-    public ExecutorService Extr;
-    public  int MaxHosts;
-    public  Map<String, Lock> ExtrLocks = new ConcurrentHashMap<>();
-    public  Map<String, Result> TPages = new ConcurrentHashMap<>();
-    public  Map<String, Set<String>> Num = new ConcurrentHashMap<>();
-    public  Semaphore ExtrLock;
-    public  Semaphore DwnlLock;
 
-    public String GetHost(String PageU) {
-        String host;
-        try {
-            host = getHost(PageU);
-        } catch (MalformedURLException e) {
-            throw new IllegalStateException("Can't get the host from url :" + PageU);
-        }
-        return host;
+public class WebCrawler implements Crawler {
+
+    AddMethod I = new AddMethod();
+
+    public WebCrawler(Downloader Dwnldr, int Dwlns, int Extrs, int PHost) throws IOException {
+        this.I.Dwnldr = Dwnldr;
+        I.ExtrLock = new Semaphore(Extrs);
+        I.DwnlLock = new Semaphore(Dwlns);
+        if (Dwlns > I.DwnlLim)
+            Dwlns = I.DwnlLim;
+        if (Extrs > I.ExtrLim)
+            Extrs = I.ExtrLim;
+        I.MaxHosts = PHost;
+        I.Extr = Executors.newWorkStealingPool(Extrs + Dwlns);
     }
-    public Document DwldPage(String PageU, String host) {
-        Document Doc = null;
-        if (TPages.containsKey(PageU)==false) {
-            DwnldLocks.putIfAbsent(PageU, new ReentrantLock());
-            DwnldLocks.get(PageU).lock();
-            try {
-                if (!TPages.containsKey(PageU)) {
-                    HostC.putIfAbsent(host, new Semaphore(MaxHosts));
-                    try {
-                        DwnlLock.acquire();
-                        HostC.get(host).acquire();
-                        try {
-                            Doc = Dwnldr.download(PageU);
-                            TPages.put(PageU, new Result(Collections.singletonList(PageU), new HashMap<>()));
-                        } catch (IOException e) {
-                            HashMap<String, IOException> Err = new HashMap<>();
-                            Err.put(PageU, e);
-                            TPages.put(PageU, new Result(new ArrayList<>(), Err));
-                        }
-                    } catch (InterruptedException e) {
-                        throw new IllegalStateException("Can't acquire the semaphore");
-                    } finally {
-                        HostC.get(host).release();
-                        DwnlLock.release();
-                    }
-                }
-            } finally {
-                DwnldLocks.get(PageU).unlock();
-            }
+    @Override
+    public Result download(String PageU, int i) {
+        if (i <= 0 || PageU == null) {
+            return I.Res;
         }
-
-        return Doc;
-    }
-    public void ExtrDoc(Document Doc, String PageU) {
-        if (Num.containsKey(PageU)==false)
+        String host = I.GetHost(PageU);
+        Document Doc = I.DwldPage(PageU, host);
+        I.ExtrDoc(Doc, PageU);
+        List<String> Dwld = new ArrayList<>();
+        HashMap<String, IOException> Err = new HashMap<>();
+        Dwld.addAll(I.TPages.get(PageU).getDownloaded());
+        Err.putAll(I.TPages.get(PageU).getErrors());
+        I.Num.getOrDefault(PageU, new HashSet<>()).stream().map(url -> (Callable<Result>) () -> download(url, i - 1)).map(I.Extr::submit).collect(Collectors.toList()).forEach(subResult ->
         {
-            ExtrLocks.putIfAbsent(PageU, new ReentrantLock());
-            ExtrLocks.get(PageU).lock();
             try {
-                if (Doc != null)
-                    if (Num.containsKey(PageU)==false)
-                    {
-                        List<String> childUrls = new ArrayList<>();
-                        try {
-                            ExtrLock.acquire();
-                            childUrls = Doc.extractLinks();
-                        } catch (IOException io) {
-                            io.printStackTrace();
-                        } catch (InterruptedException e) {
-                            throw new IllegalStateException("Unable to get semaphore");
-                        } finally {
-                            ExtrLock.release();
-                        }
-                        Num.put(PageU, new HashSet<>(childUrls));
-                    }
-            } finally
+                Result res = subResult.get();
+                Dwld.addAll(res.getDownloaded());
+                Err.putAll(res.getErrors());
+            } catch (InterruptedException ie)
             {
-                ExtrLocks.get(PageU).unlock();
+                ie.printStackTrace();
+            } catch (ExecutionException ee) {
+                ee.printStackTrace();
             }
+        });
+        return new Result(Dwld, Err);
+    }
+    @Override
+    public void close() {
+        try
+        {
+            I.Extr.shutdownNow();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String... args) throws Exception {
+          int D = 1,
+          dwnlrds = Integer.MAX_VALUE,
+          extractors = Integer.MAX_VALUE,
+          perHost = Integer.MAX_VALUE;
+        if (args.length < 1) {
+            throw new RuntimeException("Url is missing in command-line arguments.");
+        }
+        String url = args[0];
+        for (int i = 1; i < args.length && i < 5; i++) {
+
+            int n = Integer.valueOf(args[i]);
+            if (i == 1) D = n;
+            else break;
+            if (i == 2) dwnlrds = n;
+            else break;
+            if (i == 3) extractors = n;
+            else break;
+            if (i == 4) perHost = n;
+            else break;
+        }
+        Result result;
+        try (Crawler crawler = new WebCrawler(new CachingDownloader(), dwnlrds, extractors, perHost)) {
+            result = crawler.download(url, D);
+        }
+        if (Objects.nonNull(result)) {
+            result.getDownloaded().forEach(System.out::println);
+            result.getErrors().forEach((k, v) -> System.err.printf("Error on url %s: %s\n", k, String.valueOf(v)));
         }
     }
 }
-
